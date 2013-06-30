25fe0d07a491ed7b592add3219f83c02e5bebe69
John Higginson <jhigginson@gmail.com>
2013-06-30 14:58:39 -0700

    Generate the application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smilingatconfusion::Application.config.secret_key_base = '5574193f86f73a0be12f421d83b539b4490c01b8359db2beba202d14266c48d721d4e4c6ff67123125caab0417eff3f3c04a1bd6e6c85b5c82f612bf5845fa60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Smilingatconfusion::Application.config.session_store :cookie_store, key: '_smilingatconfusion_session'
