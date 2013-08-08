1bd812c1e6375f1b5c0df68861f0c9eb9431391c
John Higginson <jhigginson@gmail.com>
2013-08-07 21:29:54 -0700

    Make a User model with password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130718050000) do
+ActiveRecord::Schema.define(version: 20130808042350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 20130718050000) do
   create_table "tunings", force: true do |t|
     t.string   "name"
     t.string   "notes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
